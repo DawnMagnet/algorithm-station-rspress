0e6c9feb447fbfb470df24e0c14e7203bf85b14e
DawnMagnet <5643840+dawn_magnet@user.noreply.gitee.com>
2023-10-30 14:50:21 +0800

    update base

--- rspress.config.ts
+++ rspress.config.ts
@@ -2,17 +2,18 @@ import * as path from 'path';
 import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
-  root: path.join(__dirname, 'docs'),
-  title: 'DawnMagnet\'s Algorithm Station',
-  description: 'DawnMagnet\'s Algorithm Station.',
-  icon: "/rspress-icon.png",
-  logo: {
-    light: "/rspress-light-logo.png",
-    dark: "/rspress-dark-logo.png",
-  },
-  themeConfig: {
-    socialLinks: [
-      { icon: 'github', mode: 'link', content: 'https://github.com/web-infra-dev/rspress' },
-    ],
-  },
+    root: path.join(__dirname, 'docs'),
+    base: "/algorithm-station-rspress/",
+    title: 'DawnMagnet\'s Algorithm Station',
+    description: 'DawnMagnet\'s Algorithm Station.',
+    icon: "/rspress-icon.png",
+    logo: {
+        light: "/rspress-light-logo.png",
+        dark: "/rspress-dark-logo.png",
+    },
+    themeConfig: {
+        socialLinks: [
+            { icon: 'github', mode: 'link', content: 'https://github.com/web-infra-dev/rspress' },
+        ],
+    },
 });
